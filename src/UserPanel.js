abc85e3b27fc1333a8129d0f11080c9860b65982
Wojciech Frącz <fraczwojciech@gmail.com>
2024-05-08 12:52:38 +0200

    Finished frontend app

--- src/UserPanel.js
+++ src/UserPanel.js
@@ -4,6 +4,6 @@ export default function UserPanel({username, onLogout}) {
     return <div>
         <h2>Witaj {username}!</h2>
         <button onClick={onLogout}>Wyloguj</button>
-        <MeetingsPage/>
+        <MeetingsPage username={username}/>
     </div>;
 }
