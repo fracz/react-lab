3eb81fe9f52d151c93e1b2c189dc6fce60c38a48
Wojciech Frącz <fraczwojciech@gmail.com>
2024-05-08 12:33:37 +0200

    Conditionally show new meeting form & meetings list

--- src/meetings/MeetingsPage.js
+++ src/meetings/MeetingsPage.js
@@ -4,17 +4,23 @@ import MeetingsList from "./MeetingsList";
 
 export default function MeetingsPage() {
     const [meetings, setMeetings] = useState([]);
+    const [addingNewMeeting, setAddingNewMeeting] = useState(false);
 
     function handleNewMeeting(meeting) {
         const nextMeetings = [...meetings, meeting];
         setMeetings(nextMeetings);
+        setAddingNewMeeting(false);
     }
 
     return (
         <div>
             <h2>Zajęcia ({meetings.length})</h2>
-            <NewMeetingForm onSubmit={(meeting) => handleNewMeeting(meeting)}/>
-            <MeetingsList meetings={meetings}/>
+            {
+                addingNewMeeting
+                    ? <NewMeetingForm onSubmit={(meeting) => handleNewMeeting(meeting)}/>
+                    : <button onClick={() => setAddingNewMeeting(true)}>Dodaj nowe spotkanie</button>
+            }
+            {meetings.length > 0 && <MeetingsList meetings={meetings}/>}
         </div>
     )
 }
