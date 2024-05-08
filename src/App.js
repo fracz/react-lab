14ef5d912e73c445647cf426b5ae310a27951860
Wojciech Frącz <fraczwojciech@gmail.com>
2024-05-08 11:43:30 +0200

    UserPanel

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "milligram";
 import './App.css';
 import {useState} from "react";
 import LoginForm from "./LoginForm";
+import UserPanel from "./UserPanel";
 
 function App() {
     const [loggedIn, setLoggedIn] = useState(null);
@@ -13,23 +14,13 @@ function App() {
     }
 
     function logout() {
-        setLoggedIn(false);
-    }
-
-    let content;
-    if (loggedIn) {
-        content = <div>
-            <h2>Witaj {loggedIn}!</h2>
-            <button onClick={logout}>Wyloguj</button>
-        </div>
-    } else {
-        content = <LoginForm onLogin={login}/>;
+        setLoggedIn('');
     }
 
     return (
         <div>
             <h1>System do zapisów na zajęcia</h1>
-            {content}
+            {loggedIn ? <UserPanel username={loggedIn} onLogout={logout}/> : <LoginForm onLogin={login}/>}
         </div>
     );
 }
