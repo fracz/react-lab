6dac71d81d4b901c343785fd8e00e8a434f2e535
Student <student@d17-327e-pc-13.ipa.ii.agh.edu.pl>
2025-01-12 14:47:57 +0100

    added milligram

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
-
+import "milligram";
 
 
 function App() {
   const [title, setTitle] = useState('Walle-E');
+
+  const movies = [
+    {title: "Wall-E"},
+    {title: "Pulp Fiction"},
+    {title: "Matrix"},
+    {title: "1670"},
+];
 
   function handleChange(event){
     setTitle(event.target.value)
@@ -25,6 +32,9 @@ function App() {
   return (
     <div>
       <h1>My favorite movie {title}</h1>
+      <ul>
+        {movies.map((movie) => <li key={movie.title}>{movie.title}</li>)}
+      </ul>
       {title.length > 0 && <div>{message}</div>}
       <input type="text" value={title} onChange={handleChange}/>
       <button type='button' onClick={() => alert(title)}> Pokaż tutuł filmu </button>
