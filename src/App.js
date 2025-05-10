ef42664c71eec9aa388f323ee92130f551241ab6
Weronika Szulinska <szulinska@student.agh.edu.pl>
2025-05-10 12:00:48 +0200

    interakcje - v2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import {useState} from "react";
 function App() {
     const [email, setEmail] = useState('szulinska@agh.edu.pl');
-
+    const [authenticatedUsername, setAuthenticatedUsername] = useState(null);
     let message;
 
     if (email.length < 10) {
@@ -14,33 +14,6 @@ function App() {
         message = <div>Twój adres e-mail jest stanowczo za długi.</div>;
     }
 
-    function showAlert(){
-        alert(email);
-    }
-
-    function logIn() {
-        let idLogIn = document.getElementById("divLogIn");
-        let idLogOut = document.getElementById("divLogOut");
-
-        let logowanie = document.getElementById("logowanie").value;
-
-        if(logowanie === null || logowanie === ""){
-            alert("Aby się zalogować musisz podać twój e-mail!")
-        }else{
-            idLogIn.style.display = "none";
-            idLogOut.style.display = "block";
-        }
-
-    }
-    function logOut(){
-        let idLogIn = document.getElementById("divLogIn");
-        let idLogOut = document.getElementById("divLogOut");
-        let logowanie = document.getElementById("logowanie");
-
-        idLogIn.style.display = "block";
-        idLogOut.style.display = "none";
-        logowanie.value = "";
-    }
     function handleChange(event) {
         setEmail(event.target.value);
     }
@@ -48,15 +21,19 @@ function App() {
     return (
         <div>
             <h1>Witaj w systemie do zapisów na zajęcia</h1>
-            <div id={"divLogIn"}>
-                <label htmlFor={"logowanie"}>Zaloguj się e-mailem  </label>
-                <input id={"logowanie"} type="text" onChange={handleChange}/>
-                <button type="button" onClick={logIn}>Wchodzę</button>
-            </div>
-            <div id={"divLogOut"} style={{display: "none"}}>
-                <h2>Witaj {email}!</h2>
-                <p onClick={logOut} style={{color: "blue"}}>Wyloguj</p>
-            </div>
+            {!authenticatedUsername && (
+                <div id={"divLogIn"}>
+                    <label htmlFor={"logowanie"}>Zaloguj się e-mailem  </label>
+                    <input type="text" onChange={handleChange}/>
+                    <button type="button" onClick={() => setAuthenticatedUsername(email)}>Wchodzę</button>
+                </div>
+            )}
+            {authenticatedUsername && (
+                <div>
+                    <h4>Jesteś zalogowany jako {authenticatedUsername}</h4>
+                    <a onClick={() => setAuthenticatedUsername(null)}>Wyloguj</a>
+                </div>
+            )}
         </div>
     );
 }
