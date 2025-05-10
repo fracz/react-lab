47627cb92a303ce23704609b112ff121c429448f
Twoje Imię <twojemail@example.com>
2025-05-10 12:09:57 +0200

    Merge remote-tracking branch 'origin/reactAppLab' into reactAppLab

--- src/App.js
+++ src/App.js
@@ -6,20 +6,10 @@ import "milligram";
 function App() {
     const [email, setEmail] = useState('szulinska@agh.edu.pl');
     const [authenticatedUsername, setAuthenticatedUsername] = useState(null);
-    let message;
-
-    if (email.length < 10) {
-        message = <div>Ale masz krótki adres!</div>;
-    } else if (email.length < 15) {
-        message = <div>Twój adres e-mail jest w sam raz.</div>;
-    } else {
-        message = <div>Twój adres e-mail jest stanowczo za długi.</div>;
-    }
 
     function handleChange(event) {
         setEmail(event.target.value);
     }
-//l
     return (
         <div>
             <h1>Witaj w systemie do zapisów na zajęcia</h1>
