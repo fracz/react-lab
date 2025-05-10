fedf923804520c8e47861b31f2f555f5a6af8fca
Weronika Szulinska <szulinska@student.agh.edu.pl>
2025-05-10 10:19:50 +0200

    reaktywność

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ import {useState} from "react";
 function App() {
     const [email, setEmail] = useState('szulinska@agh.edu.pl');
 
+    let message;
+
+    if (email.length < 10) {
+        message = <div>Ale masz krótki adres!</div>;
+    } else if (email.length < 15) {
+        message = <div>Twój adres e-mail jest w sam raz.</div>;
+    } else {
+        message = <div>Twój adres e-mail jest stanowczo za długi.</div>;
+    }
+
     function handleChange(event) {
         setEmail(event.target.value);
     }
@@ -11,7 +21,8 @@ function App() {
     return (
         <div>
             <h1>System do zapisów na zajęcia</h1>
-            <h2>Twój e-mail to {email}.</h2>
+            <h2>Twój e-mail to {email}</h2>
+            {message}
             <input type="text" value={email} onChange={handleChange}/>
         </div>
     );
