b7ac58f135207671976fd6a2fb21f2c703738a9c
Twoje Imię <twojemail@example.com>
2025-05-11 10:33:41 +0200

    Merge remote-tracking branch 'origin/reactAppLab' into reactAppLab

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {useState} from "react";
 import "milligram";
 import LoginForm from "./LoginForm";
+import UserPanel from "./UserPanel";
 
 function App() {
     const [authenticatedUsername, setAuthenticatedUsername] = useState(null);
@@ -13,18 +14,18 @@ function App() {
         }
     }
 
+    function logout(){
+        setAuthenticatedUsername(null);
+    }
+
     return (
         <div>
-            <h1>Witaj w systemie do zapisów na zajęcia</h1>
-            {!authenticatedUsername && (
-                <LoginForm onLogin={(email) => login(email)}/>
-            )}
-            {authenticatedUsername && (
-                <div>
-                    <h4>Jesteś zalogowany jako {authenticatedUsername}</h4>
-                    <a onClick={() => setAuthenticatedUsername(null)}>Wyloguj</a>
-                </div>
-            )}
+            <h1>System do zapisów na zajęcia</h1>
+            {
+                authenticatedUsername
+                    ? <UserPanel username={authenticatedUsername} onLogout={logout}/>
+                    : <LoginForm onLogin={login}/>
+            }
         </div>
     );
 }
